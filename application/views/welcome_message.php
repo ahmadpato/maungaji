f770cb963e52582999048f51ea7cb38bd05dc7ad
DESKTOP-QUOH02S\M. Hidayaturrohman <mhidayaturachman@gmail.com>
2021-04-12 17:54:15 +0700

    test deploy

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -2406,7 +2406,7 @@
                 <ul><li style="color:white"><a href="" target="_blank"><i class="fa fa-envelope-square"></i><span>&nbsp;support@maungaji.co.id</span></a></li>
                 <li style="color:white"><a href="https://api.whatsapp.com/send?text=Assalamua'alaikum&phone=6281213419253" target="_blank"><i class="fa fa-whatsapp"></i><span>&nbsp; +6281213419253</span></a></li>
                 <li style="color:white"><a href="" target="_blank"><i class="fa fa-map-marker"></i><span>&nbsp; 
-                Menara 165 lantai 4 Jl TB Simatupang kav1 Cilandak Jakarta Selatan.</span></a></li></ul>
+                Menara 165 lantai 4 Jl TB Simatupang kav 1 Cilandak Jakarta Selatan.</span></a></li></ul>
             </li>
             <li>
                 <h2 class="ct-footer-list-header" style="color: #FFFFFF">Info Perusahaan</h2>
