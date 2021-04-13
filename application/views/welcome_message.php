4d038c338d11453456e7a8617096f843d9ea8e6f
Mahesa Muhammad Elba <mahessassin888@gmail.com>
2021-04-14 06:14:19 +0700

    Fix icon list inside footer

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -2396,18 +2396,18 @@
             <li>
                 <h2 class="ct-footer-list-header" style="color: #FFFFFF">Follow Kami</h2>
                 <ul class="fa-ul">
-                    <li><a href="https://www.instagram.com/maungaji/" target="_blank"><i class="fa-li fa fa-instagram"></i>maungaji</a></li>
-                    <li><a href="https://www.facebook.com/maungaji/" target="_blank"><i class="fa-li fa fa-facebook"></i>maungaji</a></li>
-                    <li><a href="https://twitter.com/MaungajiIndo" target="_blank"><i class="fa-li fa fa-twitter"></i>maungajiIndo</a></li>
-                    <li><a href="https://www.youtube.com/channel/UCIp6c79qyzU98400W253WYQ" target="_blank"><i class="fa-li fa fa-youtube"></i>maungaji official</a></li>
+                    <li><a href="https://www.instagram.com/maungaji/" target="_blank"><i class="fa-li fa fa-instagram text-persian-pink"></i>maungaji</a></li>
+                    <li><a href="https://twitter.com/MaungajiIndo" target="_blank"><i class="fa-li fa fa-twitter text-carolina-blue"></i>maungajiIndo</a></li>
+                    <li><a href="https://www.youtube.com/channel/UCIp6c79qyzU98400W253WYQ" target="_blank"><i class="fa-li fa fa-youtube text-orange-soda"></i>maungaji official</a></li>
+                    <li><a href="https://www.facebook.com/maungaji/" target="_blank"><i class="fa-li fa fa-facebook text-carolina-blue"></i>maungaji</a></li>
                 </ul>
             </li>
             <li>
                 <h2 class="ct-footer-list-header" style="color: #FFFFFF">Hubungi Kami</h2>
                 <ul class="fa-ul text-white">
-                    <li><i class="fa-li fa fa-envelope-square"></i>support@maungaji.co.id</li>
-                    <li><a href="https://api.whatsapp.com/send?text=Assalamua'alaikum&phone=6281213419253" target="_blank"><i class="fa-li fa fa-whatsapp"></i>+6281213419253</a></li>
-                    <li><i class="fa-li fa fa-map-marker"></i>Menara 165 lantai 4 Jl TB Simatupang kav 1 Cilandak Jakarta Selatan</li>
+                    <li><i class="fa-li fa fa-envelope text-maximum-yellow"></i>support@maungaji.co.id</li>
+                    <li><a href="https://api.whatsapp.com/send?text=Assalamua'alaikum&phone=6281213419253" target="_blank"><i class="fa-li fa fa-whatsapp text-medium-sea-green"></i>+6281213419253</a></li>
+                    <li><i class="fa-li fa fa-map-marker text-tart-orange"></i>Menara 165 lantai 4 Jl TB Simatupang kav 1 Cilandak Jakarta Selatan</li>
                 </ul>
             </li>
             <li>
@@ -2416,7 +2416,7 @@
                     <li><a href="https://maungaji-2019.s3-ap-southeast-1.amazonaws.com/additional/tnc.html">Kebijakan Privasi</a></li>
                     <li><a href="https://maungaji-2019.s3-ap-southeast-1.amazonaws.com/additional/tnc.html">Syarat dan Ketentuan</a></li>
                     <li><a href="">Karir</a></li>
-                    <li><a href="https://maungaji.co.id/artikel">blog</a></li>
+                    <li><a href="https://maungaji.co.id/artikel">Blog</a></li>
                 </ul>
                 <br/>
                 <br/>
