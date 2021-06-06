e27896cad37907b7dd64a0fe2a98e7a2934cc066
DESKTOP-QUOH02S\M. Hidayaturrohman <mhidayaturachman@gmail.com>
2021-06-06 21:05:51 +0700

    remove font awesome 5 cdn

--- application/views/home.php
+++ application/views/home.php
@@ -154,7 +154,7 @@
                     </div>   
                     <div class="text-center">
                         <p class="text-register-student text-center" style="color: #3597D4;">
-                        <a href="https://linktr.ee/Maungaji" target="_blank">Daftar Jadi Santri Ngaji <i class="fas fa-arrow-right"></i>
+                        <a href="https://linktr.ee/Maungaji" target="_blank">Daftar Jadi Santri Ngaji <i class="fa fa-arrow-right"></i>
                         </a>
                         </p>
                     </div>   
@@ -186,7 +186,7 @@
                     </div>   
                     <div class="text-center">
                         <p class="text-register-teacher text-center" style="color: #3597D4;">
-                        <a href="https://play.google.com/store/apps/details?id=com.maungaji.guruapp" target="_blank">Daftar Jadi Guru Ngaji <i class="fas fa-arrow-right"></i></a>
+                        <a href="https://play.google.com/store/apps/details?id=com.maungaji.guruapp" target="_blank">Daftar Jadi Guru Ngaji <i class="fa fa-arrow-right"></i></a>
                         </p>
                     </div>   
                 </div>
