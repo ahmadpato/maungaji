e72950dcfcaa092636e594d44520043f8fe8c0da
DESKTOP-QUOH02S\M. Hidayaturrohman <mhidayaturachman@gmail.com>
2021-07-04 11:12:58 +0700

    change url

---
+++ application/views/header.php
@@ -0,0 +1,111 @@
+<?php
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<!--important meta tags -->
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+ <!--Title -->
+<title>Maungaji | Pesan Guru Ngaji Privat</title>
+
+ <!--Basic SEO -->
+<meta name="description" content="Privat ngaji di tempat yang kamu tentukan, murah, mudah dan sesuai syariah.">
+<meta name="keywords" content="guru ngaji, guru ngaji online, belajar ngaji, belajar iqro, belajar al-quran, belajar tahsin, hafidz qur'an, aplikasi ngaji online, belajar iqro online, belajar al-quran online, ngaji online, privat ngaji, guru ngaji, guru ngaji profesional, privat ngaji online, ngaji, guru ngaji berkualitas">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+
+ <!--Favicon -->
+<link rel="icon" type="image/png" href="<?php echo base_url('/img/favicon/favicon-32x32.png') ?>">
+
+ <!--Custom CSS -->
+<link rel="stylesheet" href="/css/maungaji-style.min.css?version=1.0.6">
+
+<!--Fontpoppins -->
+<link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+
+<!--Rakkas -->
+<link href='https://fonts.googleapis.com/css?family=Rakkas' rel='stylesheet'>
+
+ <!--Facebook Pixel Code -->
+<script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '523156341768942');
+  fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=523156341768942&ev=PageView&noscript=1"
+/></noscript>
+ <!--End Facebook Pixel Code -->
+
+ <script data-ad-client="ca-pub-6962543154354144" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+
+</head>
+
+<body data-spy="scroll" data-target=".navbar" data-offset="65">
+     <!--Header -->
+    <header>
+        <nav class="navbar navbar-fixed-top">
+            <div class="container-fluid">
+                <div class="site-nav-wrapper">
+                    <div class="navbar-header">
+                         <!--Mobile Menu Open Button -->
+                        <span id="mobile-nav-open-btn">&#9776;</span>
+                         <!--Logo -->
+                        <a class="navbar-brand" href="/">
+                            <img class="logo-non-text" src="/img/logo/Mau-Ngaji-Logo-transparent.png" alt="logo" width="auto" class="img-responsive" height="50px" style="margin-top: -10px;margin-left: 72px;">
+                        </a>
+                         <!--Logo -->
+                        <a class="navbar-brand-mobile" href="/">
+                            <img src="/img/logo/Mau-Ngaji-Logo-transparent.png" alt="maungaji" border="0" class="img-responsive">
+                        </a>
+                    </div>
+
+                    <!--Main Menu -->
+                    <div class="container">
+                        <div class="collapse navbar-collapse">
+                            <ul class="nav navbar-nav pull-right">
+                                <li><a href="/">HOME PAGE</a></li>
+                                <li><a href="https://blog.maungaji.co.id">ARTICLE</a></li>
+                                <li><a href="/army">ARMY LIST</a></li>
+                                <li><a href="/teacher">TEACHER LIST</a></li>
+                                <li><a href="https://docs.google.com/forms/d/e/1FAIpQLSfXTB4RKaDXoB01ld4tA8TFSiQsu-u790F4bmG9WTeCYf8bIA/viewform" target="_blank">
+                                <div class="teacher">
+                                    MAUNGAJI ARMY
+                                </div>
+                                </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <!--Mobile Menu -->
+                    <div id="mobile-nav">
+                         <!--Mobile Menu Close Button -->
+                        <span id="mobile-nav-close-btn">&times;</span>
+                        <div id="mobile-nav-content">
+                            <ul class="nav">
+                                <li><a href="/">HOME PAGE</a></li>
+                                <li><a href="https://blog.maungaji.co.id">ARTICLE</a></li>
+                                <li><a href="/army">ARMY LIST</a></li>
+                                <li><a href="/teacher">TEACHER LIST</a></li>
+                                <li><a href="https://docs.google.com/forms/d/e/1FAIpQLSfXTB4RKaDXoB01ld4tA8TFSiQsu-u790F4bmG9WTeCYf8bIA/viewform" class="teacher" target="_blank">MAUNGAJI ARMY</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <!--Header Ends -->
